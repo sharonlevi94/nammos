89db5cbd8e022fe473b155817a5d2772defb9e86
sharonlevi94 <sharonlevy96@gmail.com>
2022-12-31 19:11:52 +0200

    add soon

--- pages/home.vue
+++ pages/home.vue
@@ -9,11 +9,16 @@
           class="card"
           xs12
           mb-2
-          @click="$router.push({name: item && item.to})">
-          <i
-            :class="['icon', item && item.icon, 'xl', 'ml-2']"
-            :style="item && item.iconStyles.join('')"/>
-          <span>{{ item && item.name }}</span>
+          @click="!(item && item.soon) ? $router.push({name: item && item.to}) : null">
+          <div class="d-flex justify-space-between" style="width: 100%">
+            <div class="d-flex">
+              <i
+                :class="['icon', item && item.icon, 'xl', 'ml-2']"
+                :style="item && item.iconStyles.join('')"/>
+              <span>{{ item && item.name }}</span>
+            </div>
+            <div v-if="item && item.soon" class="soon">בקרוב!</div>
+          </div>
         </v-flex>
       </v-layout>
     </v-container>
@@ -30,8 +35,8 @@ export default {
       items: [
         { name: 'הורדת סירה', icon: 'boat', to: 'pick-date', iconStyles: [], admin: false },
         { name: 'העלאת סירה', icon: 'boat', to: 'queue', iconStyles: ['transform: scaleX(-1);'], admin: false },
-        { name: 'הפרופיל שלי', icon: 'user', to: 'profile', iconStyles: [], admin: false },
-        { name: 'תקנון ומדיניות', icon: 'rules', to: 'privacy-policy', iconStyles: [], admin: false },
+        { name: 'הפרופיל שלי', icon: 'user', to: 'profile', iconStyles: [], admin: false, soon: true },
+        { name: 'תקנון ומדיניות', icon: 'rules', to: 'privacy-policy', iconStyles: [], admin: false, soon: true },
         { name: 'איזור ניהול', icon: 'rules', to: 'admin', iconStyles: [], admin: true },
       ]
     }
@@ -40,5 +45,7 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-
+.soon {
+  color: $primary;
+}
 </style>
